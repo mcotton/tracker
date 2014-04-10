afb632b60be44639019b14ad498a4cfa96e246ba
Mark Cotton <tifosigoon@gmail.com>
2014-04-10 18:50:23 -0500

    working on adding CORS support, need to test

--- app.js
+++ app.js
@@ -26,6 +26,12 @@ app.configure(function(){
   app.use(express.static(path.join(__dirname, 'public')));
 });
 
+app.all('*', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next();
+});
+
 app.get('/', routes.index);
 
 server.listen(app.get('port'), function(){
